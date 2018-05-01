056a5ca7a03b6ffe4f25bb7c76eda7ddc3533991
Filip Stenström <elt13fst@student.lu.se>
2018-05-01 10:51:57 +0200

    Added zone loading of GUI frame updates

--- WorldBossTimers.lua
+++ WorldBossTimers.lua
@@ -7,6 +7,8 @@ local _, L = ...;
 WBT = LibStub("AceAddon-3.0"):NewAddon("WBT", "AceConsole-3.0");
 
 local gui;
+local boss_death_frame;
+local boss_combat_frame;
 
 local defaults = {
     global = {
@@ -107,11 +109,159 @@ local function getSpawnTime(name)
     return formatTimeSeconds(spawnTimeSec);
 end
 
+local function IsBossZone()
+    current_zone = GetZoneText();
+
+    is_boss_zone = false;
+    for name, boss in pairs(bosses) do
+        if boss.zone == current_zone then
+            is_boss_zone = true;
+        end
+    end
+
+    return is_boss_zone;
+end
+
 local function isDead(name)
     if WBT.db.global.boss[name] == nil then
         return false;
     end
     return getSpawnTimeSec(name) >= 0;
+end
+
+local function AnyDead()
+    any_dead = false;
+    for name, boss in pairs(bosses) do
+        if isDead(name) then
+            any_dead = true;
+        end
+    end
+    return any_dead;
+end
+
+local function ShouldShowGUI()
+    return AnyDead() or IsBossZone();
+end
+
+local function getBossNames()
+    local boss_names = {};
+    local i = 1; -- Don't start on index = 0... >-<
+    for name, _ in pairs(bosses) do
+        boss_names[i] = name;
+        i = i + 1;
+    end
+
+    return boss_names;
+end
+
+local function initGUI()
+
+    local AceGUI = LibStub("AceGUI-3.0"); -- Need to create AceGUI 'OnInit or OnEnabled'
+    gui = AceGUI:Create("Window");
+
+    gui:SetWidth(200);
+    gui:SetHeight(100);
+    gui:SetCallback("OnClose",function(widget) AceGUI:Release(widget) end);
+    gui:SetTitle("World Boss Timer");
+    gui:SetLayout("List");
+    gui:EnableResize(false);
+    gui.frame:SetFrameStrata("LOW");
+
+    function gui:update()
+        self:ReleaseChildren();
+
+        for name, boss in pairs(WBT.db.global.boss) do
+            if isDead(name) then
+                local label = AceGUI:Create("InteractiveLabel");
+                label:SetWidth(170);
+                label:SetText(getColoredBossName(name) .. ": " .. getSpawnTime(name));
+                label:SetCallback("OnClick", function() WBT:Print(name) end);
+                -- Add the button to the container
+                self:AddChild(label);
+            end
+        end
+    end
+
+    function gui:initPosition()
+        gui_position = WBT.db.char.gui_position;
+        local gp;
+        if gui_position ~= nil then
+            gp = gui_position;
+        else
+            gp = {
+                point = "Center",
+                relativeToName = "UIParrent",
+                realtivePoint = nil,
+                xOfs = 0,
+                yOfs = 0,
+            }
+        end
+        self:ClearAllPoints();
+        self:SetPoint(gp.point, relativeTo, gp.xOfs, gp.yOfs);
+    end
+
+    local function recordGUIPositioning()
+        local function saveGuiPoint()
+            point, relativeTo, relativePoint, xOfs, yOfs = gui:GetPoint();
+            WBT.db.char.gui_position = {
+                point = point,
+                relativeToName = "UIParrent",
+                relativePoint = relativePoint,
+                xOfs = xOfs,
+                yOfs = yOfs,
+            };
+            -- print(WBT.db.char.gui_position.point, WBT.db.char.gui_position.relativeToName, WBT.db.char.gui_position.relativePoint, WBT.db.char.gui_position.xOfs, WBT.db.char.gui_position.yOfs);
+        end
+        hooksecurefunc(gui.frame, "StopMovingOrSizing", saveGuiPoint);
+    end
+
+    gui:update();
+
+    gui:initPosition();
+
+    gui:Show();
+
+    recordGUIPositioning();
+end
+
+local function RegisterEvents()
+    boss_death_frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+    boss_combat_frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+end
+
+local function UnregisterEvents()
+    boss_death_frame:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+    boss_combat_frame:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+end
+
+local function ShowGUI()
+    if gui ~= nil then
+        gui:Hide();
+        gui = nil;
+    end
+    initGUI();
+end
+
+local function HideGUI()
+    if gui ~= nil then
+        gui:Hide();
+        gui = nil;
+    end
+end
+
+local function RestartGUI()
+    HideGUI();
+    ShowGUI();
+end
+
+local function UpdateGUIVisibility()
+    if ShouldShowGUI() then
+        RegisterEvents();
+        RestartGUI();
+    else
+        UnregisterEvents();
+        HideGUI();
+    end
 end
 
 local function startWorldBossDeathTimer(...)
@@ -133,16 +283,17 @@ local function startWorldBossDeathTimer(...)
         local until_time = GetServerTime() + time;
         local UpdateInterval = freq;
         boss.timer:SetScript("OnUpdate", function(self, elapsed)
-                if self.TimeSinceLastUpdate == nil then 
+                if self.TimeSinceLastUpdate == nil then
                     self.TimeSinceLastUpdate = 0;
                 end
-                self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed; 	
+                self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed;
 
                 if (self.TimeSinceLastUpdate > UpdateInterval) then
                     self.remaining_time = until_time - GetServerTime();
                     if self.remaining_time < 0 or self.kill then
                         FlashClientIcon();
                         killUpdateFrame(self);
+                        UpdateGUIVisibility();
                     end
 
                     if gui ~= nil then
@@ -161,34 +312,40 @@ local function startWorldBossDeathTimer(...)
         end
     end
 end
-    
-local function registerWorldBossDeaths()
-    local boss_death_frame = CreateFrame("Frame");
-    boss_death_frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+
+local function InitDeathTrackerFrame()
+    if boss_death_frame ~= nil then
+        return
+    end
+
+    boss_death_frame = CreateFrame("Frame");
     boss_death_frame:SetScript("OnEvent", function(event, ...)
              local timestamp, type, hideCaster, sourceGUID, sourceName, sourceFlags, sourceFlags2, destGUID, destName, destFlags, destFlags2 = select(2, ...);
-             if type == "UNIT_DIED" and isBoss(destName) then 
+             if type == "UNIT_DIED" and isBoss(destName) then
                  setDeathTime(GetServerTime(), destName); -- Don't use timestamp from varags. It's not synchronized with server time.
                  startWorldBossDeathTimer(destName);
              end
-        end); 
+        end);
 end
 
-local function scanWorldBossCombat() 
-    local boss_combat_frame = CreateFrame("Frame");
-    boss_combat_frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+local function InitCombatScannerFrame()
+    if boss_combat_frame ~= nil then
+        return
+    end
+
+    boss_combat_frame = CreateFrame("Frame");
 
     local time_out = 60*2; -- Legacy world bosses SHOULD die in this time.
     boss_combat_frame.t_next = 0;
 
-    function boss_combat_frame:doScanWorldBossCombat(event, ...) 
-        -- NOTE: I don't know why registerWorldBossDeaths gets one extra input arg in varags. Seems to be 'event'.
+    function boss_combat_frame:doScanWorldBossCombat(event, ...)
+        -- NOTE: I don't know why InitDeathTrackerFrame gets one extra input arg in varags. Seems to be 'event'.
         local timestamp, type, hideCaster, sourceGUID, sourceName, sourceFlags, sourceFlags2, destGUID, destName, destFlags, destFlags2 = select(1, ...);
 
         local t = GetServerTime();
 
         if isBoss(destName) and t > self.t_next then
-            WBT:Print(getColoredBossName(destName) .. " is now engaged in combat!"); 
+            WBT:Print(getColoredBossName(destName) .. " is now engaged in combat!");
             local soundfile = bosses[destName].soundfile;
             PlaySoundFile(soundfile, "Master");
             FlashClientIcon();
@@ -200,17 +357,6 @@ local function scanWorldBossCombat()
 end
 
 function WBT:OnInitialize()
-end
-
-local function getBossNames()
-    local boss_names = {};
-    local i = 1; -- Don't start on index = 0... >-<
-    for name, _ in pairs(bosses) do
-        boss_names[i] = name;
-        i = i + 1;
-    end
-    
-    return boss_names;
 end
 
 local function printKilledBosses()
@@ -268,78 +414,6 @@ local function announceSpawnTime(currentZoneOnly)
     end
 end
 
-
-local function initGUI()
-
-    local AceGUI = LibStub("AceGUI-3.0"); -- Need to create AceGUI 'OnInit or OnEnabled' 
-    gui = AceGUI:Create("Window");
-
-    gui:SetWidth(200);
-    gui:SetHeight(100);
-    gui:SetCallback("OnClose",function(widget) AceGUI:Release(widget) end);
-    gui:SetTitle("World Boss Timer");
-    gui:SetLayout("List");
-    gui:EnableResize(false);
-    gui.frame:SetFrameStrata("LOW");
-
-    function gui:update()
-        self:ReleaseChildren();
-
-        for name, boss in pairs(WBT.db.global.boss) do
-            if isDead(name) then
-                local label = AceGUI:Create("InteractiveLabel");
-                label:SetWidth(170);
-                label:SetText(getColoredBossName(name) .. ": " .. getSpawnTime(name));
-                label:SetCallback("OnClick", function() WBT:Print(name) end);
-                -- Add the button to the container
-                self:AddChild(label);
-                --WBT:Print(label:IsShown());
-            end
-        end
-    end
-
-    function gui:initPosition()
-        gui_position = WBT.db.char.gui_position;
-        local gp;
-        if gui_position ~= nil then
-            gp = gui_position;
-        else
-            gp = {
-                point = "Center",
-                relativeToName = "UIParrent",
-                realtivePoint = nil,
-                xOfs = 0,
-                yOfs = 0,
-            }
-        end
-        self:ClearAllPoints();
-        self:SetPoint(gp.point, relativeTo, gp.xOfs, gp.yOfs);
-    end
-
-    local function recordGUIPositioning()
-        local function saveGuiPoint()
-            point, relativeTo, relativePoint, xOfs, yOfs = gui:GetPoint();
-            WBT.db.char.gui_position = {
-                point = point,
-                relativeToName = "UIParrent",
-                relativePoint = relativePoint,
-                xOfs = xOfs,
-                yOfs = yOfs,
-            };
-            -- print(WBT.db.char.gui_position.point, WBT.db.char.gui_position.relativeToName, WBT.db.char.gui_position.relativePoint, WBT.db.char.gui_position.xOfs, WBT.db.char.gui_position.yOfs);
-        end
-        hooksecurefunc(gui.frame, "StopMovingOrSizing", saveGuiPoint);
-    end
-
-    gui:update();
-    
-    gui:initPosition();
-
-    gui:Show();
-
-    recordGUIPositioning();
-end
-
 local function ResetKillInfo()
     WBT:Print("Reseting all kill info.");
     for k, v in pairs(WBT.db.global.boss) do
@@ -348,26 +422,13 @@ local function ResetKillInfo()
     end
 end
 
-local function ShowGUI()
-    if gui ~= nil then
-        gui:Hide();
-        gui = nil;
-    end
-    initGUI();
-end
-
-local function HideGUI()
-    gui:Hide();
-    gui = nil;
-end
-
 local function slashHandler(input)
 
     -- print(input);
     -- input = input:trim();
     arg1, arg2 = strsplit(" ", input);
     -- print(arg1, arg2);
-    
+
     if arg1 == "hide" then
         HideGUI();
     elseif arg1 == "show" then
@@ -392,16 +453,34 @@ local function slashHandler(input)
 
 end
 
+local function StartVisibilityHandler()
+    local visibilty_handler_frame = CreateFrame("Frame");
+    visibilty_handler_frame:RegisterEvent("ZONE_CHANGED_NEW_AREA");
+    visibilty_handler_frame:SetScript("OnEvent",
+        function(e, ...)
+            UpdateGUIVisibility();
+        end
+    );
+end
+
 function WBT:OnEnable()
 	WBT.db = LibStub("AceDB-3.0"):New("WorldBossTimersDB", defaults);
     -- self.db.global = defaults.global; -- Resets the global profile in case I mess up the table
     -- /run for k, v in pairs(WBT.db.global) do WBT.db.global[k] = nil end -- Also resets global profile, but from in-game
 
-    registerWorldBossDeaths(); -- Todo: make sure this can't be called twice in same session
-    startWorldBossDeathTimer(unpack(getBossNames()));
-    scanWorldBossCombat();
-
+    InitDeathTrackerFrame(); -- Todo: make sure this can't be called twice in same session
+    InitCombatScannerFrame();
     initGUI();
+
+    if AnyDead() or IsBossZone() then
+        RegisterEvents();
+        startWorldBossDeathTimer(unpack(getBossNames()));
+        ShowGUI();
+    else
+        HideGUI();
+    end
+
+    StartVisibilityHandler();
 
     self:RegisterChatCommand("wbt", slashHandler);
     self:RegisterChatCommand("worldbosstimers", slashHandler);
