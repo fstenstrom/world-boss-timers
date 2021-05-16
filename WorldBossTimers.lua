696c09011653e01248e2f8b01ae73d76588f5479
Filip Stenstrom <filip.stenstrom.4488@gmail.com>
2021-05-16 14:06:05 +0200

    Clarify output of Info message

--- WorldBossTimers.lua
+++ WorldBossTimers.lua
@@ -369,7 +369,7 @@ local function GetSafeSpawnAnnouncerWithCooldown()
         local t_now = GetServerTime();
 
         if not kill_info then
-            Logger.Info("No timer found for current location/realm/warmode.");
+            Logger.Info("No timer found for current location+realm+warmode.");
             return announced;
         end
         if not ((t_last_announce + 1) <= t_now) then
