ac6e5cc6ea6d79dc5ea5cdd42a9f295a9a4b4b84
Filip Stenstrom <filip.stenstrom.4488@gmail.com>
2024-07-27 10:45:39 +0000

    Update timer request message

--- WorldBossTimers.lua
+++ WorldBossTimers.lua
@@ -152,7 +152,7 @@ local g_current_shard_id;
 
 local CHANNEL_ANNOUNCE = "SAY";
 local SERVER_DEATH_TIME_PREFIX = "WorldBossTimers:";
-local CHAT_MESSAGE_TIMER_REQUEST = "Could you please share WorldBossTimers kill data?";
+local CHAT_MESSAGE_TIMER_REQUEST = "Could someone share WBT timer?";
 
 WBT.defaults = {
     global = {
